b05e4093120c9be47b0b6bfd655879ee0ac59b21
NikitaBorah <nikitaborah3@gmail.com>
2024-08-30 14:26:30 +0530

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const Home = async () => {
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center"> 
             <p className="small-text">
-              Smart Shopping Starts Here
+            Smart Shopping Made Easy
               <Image 
                 src="/assets/icons/arrow-right.svg"
                 alt="arrow-right"
@@ -23,12 +23,12 @@ const Home = async () => {
             </p>
 
             <h1 className="head-text">
-              Unleash the Power of
+              Welcome To
               <span className="text-primary"> PriceTracker</span>
             </h1>
 
             <p className="mt-6">
-              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+            Simple, self-serve tools to help you track prices, save money, and make smarter shopping decisions.
             </p>
             <Searchbar />
           </div>
